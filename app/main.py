903d9a4689725baa68f40ae2cf36da7f3d843677
Mt546 <muhsinesad@gmail.com>
2023-04-20 02:05:45 +0300

    expose_00

--- app/main.py
+++ app/main.py
@@ -22,5 +22,5 @@ def predict(data: MyData):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run(app,host="127.0.0.1", port=8000)
+#if __name__ == "__main__":
+ #   uvicorn.run(app,host="127.0.0.1", port=8000)
